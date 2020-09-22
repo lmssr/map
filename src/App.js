b404c90890a877bbdab17fc2108be05c548e780c
Luc Mosser <luc.mosser86@orange.fr>
2020-09-22 11:23:35 +0200

    Creating the map

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
+import "./App.css";
 import React from 'react';
-import logo from './logo.svg';
+import ReactMapGL, { GeolocateControl, NavigationControl } from "react-map-gl";
+import Geocoder from "react-map-gl-geocoder";
+import "react-map-gl-geocoder/dist/mapbox-gl-geocoder.css";
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+class App extends React.Component {
+  state = {
+    viewport: {
+      latitude: 40.7250863,
+      longitude: -73.9773600,
+      zoom: 0
+    }
+  };
+
+  myMap = React.createRef();
+
+  render() {
+    console.log(this.state.viewport)
+    return (
+      <div>
+        <ReactMapGL
+          ref={this.myMap}
+          {...this.state.viewport}
+          width="100vw"
+          height="100vh"
+          onViewportChange={viewport => this.setState({ viewport })}
+          mapStyle="mapbox://styles/mapbox/satellite-v9"
+          mapboxApiAccessToken="pk.eyJ1IjoibG1zc3IiLCJhIjoiY2swNnF6eGRqMzNodDNpcWR2MTN1Y2pnZSJ9.-Cdm2-HPW8myBQjzq3AO4Q"
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          <Geocoder
+            mapRef={this.myMap}
+            mapboxApiAccessToken="pk.eyJ1IjoibG1zc3IiLCJhIjoiY2swNnF6eGRqMzNodDNpcWR2MTN1Y2pnZSJ9.-Cdm2-HPW8myBQjzq3AO4Q"
+            />
+          <GeolocateControl />
+          <NavigationControl showCompass={false} showZoom={false}/>
+        </ReactMapGL>
+      </div>
+      )
+  }
 }
+
 
 export default App;
